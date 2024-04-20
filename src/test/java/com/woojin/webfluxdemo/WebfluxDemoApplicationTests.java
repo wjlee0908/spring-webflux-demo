509a84b05af71fd960e7b03f79a87f97de88b026
Woojin Lee <dev.woojin.lee@gmail.com>
2024-04-21 02:55:26 +0900

    Initial commit

---
+++ src/test/java/com/woojin/webfluxdemo/WebfluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woojin.webfluxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
